bb63a77f54f02b3d30cb0971be64e69a959b3455
Eric Wooley <ericwooley@gmail.com>
2014-04-02 23:46:42 -0700

    Began hooking up the api, added steps

--- LessonPlanner/app/scripts/capstone.js
+++ LessonPlanner/app/scripts/capstone.js
@@ -1,3 +1,10 @@
+function finishLoading(){
+	$("#ajax_loader").fadeOut();
+}
+function startLoading(){
+	$("#ajax_loader").fadeIn();	
+}
+
 $(function(){
 	Parse.$ = $;
 	Parse.initialize('29wOKycz8F6JaRXHNttPXiynbGOFKUqOYAZvKZVc',
@@ -6,93 +13,181 @@ $(function(){
 	//////////////////////////////////////////////////////////////
 	/// Objects
 	//////////////////////////////////////////////////////////////
-	var Lesson = Parse.Object.extend('Lesson', {
-		defaults: {
-			Title: 'Lesson Title',
-			Rating: 0,
-			Description: 'Lesson Description',
-			Author: 'Author Name',
-			Goal: 'The Goal of this lesson',
-			AttemptCount: 0,
-			MaxAttempts: 0,
-			Score: 0,
-			SubLessons:{
-				Title: 'Sub Lesson Title',
-				SubLessonGoal: 'The goal of this sublesson',
-				Steps: [
-					{
-						GoalOfStep: 'Step Goal',
-						ExpectedResult: 10,
-						Hints: [
-							'hint 1',
-							'hint 2'
-						],
-						Goal: 'Step Goal',
-						Status: 'unstarted',
-						Initialtext: 'This is the text you want them to start with.'
-					}
-				]
-			}
+	var Course = Parse.Object.extend('Course', {}, {
+		Create: function(){
+			var c = new Course();
+			c.set('Author', Parse.User.current());
+			return c;
+		}
+	});
+	var Lesson = Parse.Object.extend('Lesson');
+	var Step = Parse.Object.extend('Step');
+
+	var Profile = Parse.Object.extend('Profile', {},{
+		Create: function(){
+			var c = new Profile();
+			c.set('User', Parse.User.current());
+			return c;
 		}
 	});
 
+
+
+	//var query = new Parse.Query(Course);
+	//query.equalTo("Author", Parse.User.current());
+	//query.find({
+	//	success: function(results){
+	//		console.log("RESULTS",JSON.stringify(results));
+	//	}
+	//})
 
 	//////////////////////////////////////////////////////////////
 	/// Collections
 	//////////////////////////////////////////////////////////////
 
 
+	var Courses = Parse.Collection.extend({
+		Model: Course
+	});
+
 	var Lessons = Parse.Collection.extend({
 		Model: Lesson
+	});
+
+	var Steps = Parse.Collection.extend({
+		Model: Step
 	});
 
 	//////////////////////////////////////////////////////////////
 	/// Views
 	//////////////////////////////////////////////////////////////
+	
+	
 
-	var LessonView = Parse.View.extend({
-		el:('.content'),
-		template: _.template($('#LessonTemplate').html()),
-		initialize: function(){
-			this.render();
-		},
-		render: function(){
-			this.$el.html(this.template({Lesson: this.model}));
-		}
-	});
 
-	var LessonSelectorView = Parse.View.extend({
-		el: '#LessonSelector',
-		initialize: function(ops){
-			this.lessons = ops.lessons;
-			this.render();
-		},
-		render: function(){
-			$(this.$el).html(_.template($('#LessonSelectorTemplate').html(), {Lessons: this.lessons.toJSON()}));
-		}
-	});
-
-	var GetStartedView = Parse.View.extend({
+	var SelectCourseView = Parse.View.extend({
 		events: {
-			'click .newLessonButton': 'newLesson'
+			'click .newCourseButton': 'newCourse'
 		},
-		lessons: new Lessons(),
+		courses: new Courses(),
 		el: '.content',
 		initialize: function() {
 
 			this.render();
-			_.bindAll(this, 'newLesson')
+			_.bindAll(this, 'newCourse');
 		},
 		render: function(){
-			this.$el.html(_.template($('#getStartedTemplate').html()));
-			new LessonSelectorView({lessons: this.lessons});
+			this.$el.html(_.template($('#SelectCourseView').html()));
+			new CourseDropdownView({courses: this.courses});
 		},
-		newLesson: function(){
-			var lesson = new Lesson();
-			this.lessons.add(lesson);
-			new LessonView({model: lesson});
+		newCourse: function(){
+			var course = new Course.Create();
+			this.courses.add(course);
+			new CourseView({model: course});
 		}
 	});
+	var CourseDropdownView = Parse.View.extend({
+		el: '#CourseSelector',
+		initialize: function(ops){
+			this.courses = ops.courses;
+			this.render();
+		},
+		render: function(){
+			$(this.$el).html(_.template($('#CourseDropdownTemplate').html(), {Courses: this.courses.toJSON()}));
+		}
+	});
+	var CourseView = Parse.View.extend({
+		events: {
+			'click #newLesson': 'newLesson'
+		},
+		el:('.content'),
+		template: _.template($('#CourseTemplate').html()),
+		initialize: function(opts){
+			this.render();
+			console.log(this.model.toJSON());
+			//console.log(this.course.toJSON());
+		},
+		render: function(){
+			this.$el.html(this.template({Course: this.model}));
+		},
+		newLesson: function(){
+			var that = this;
+			var l = new Lesson();
+			startLoading();
+			l.save(null, {
+				success: function(){
+					var arr = that.model.get('Lessons') || [];
+					arr.push(l.id);
+					that.model.set('Lessons', arr);
+					that.model.save(null, {
+						success: function(){
+							console.log('saved lesson');
+							finishLoading();
+						}
+					});
+					new LessonView({model: l, lessonNumber: that.model.get('Lessons').length, el: '#lessons' });
+				},
+				error: function(){
+					alert('error');
+					finishLoading();
+				}
+			});
+		}
+	});
+
+
+
+	var LessonView = Parse.View.extend({
+		events: {},
+		tag:'li',
+		newStep: function(){
+			var s = new Step();
+			s.save(null, {
+				success: function(){
+					this.model.steps.push()
+				}
+			})
+			new StepView({
+				el:'#steps-'+this.lessonNumber,
+				step: s,
+				lessonNumber: this.lessonNumber
+			});
+
+		},
+		template: _.template($('#LessonTemplate').html()),
+		initialize: function(){
+			this.lessonNumber = this.options.lessonNumber;
+			this.undelegateEvents();
+			this.events['click #newStep-'+this.options.lessonNumber] = this.newStep;
+			this.delegateEvents();
+			//this.lesson = this.options.lesson;
+			this.render();
+			_.bindAll(this, 'newStep');
+		},
+		render: function(){
+			this.$el.append(this.template({
+				Lesson: this.model,
+				lessonNumber: this.lessonNumber
+			}));
+		}
+	});
+
+	var StepView = Parse.View.extend({
+		template: _.template($('#StepTemplate').html()),
+		initialize: function(){
+			console.log(this);
+			this.step = this.options.step;
+			// this.$el = $('#steps-'+ops.lessonNumber);
+			console.log("initialize:", this.options.lessonNumber, this.el);
+			this.render();
+		},
+		render: function(){
+			this.$el.append(this.template({Step: this.step}));
+			// console.log("scrollTo: ", this.$el)
+			// scrollTo($(this.$el));
+		}
+	});
+
 
 	var LogInView = Parse.View.extend({
 		events: {
@@ -116,10 +211,32 @@ $(function(){
 
 			Parse.User.logIn(username, password, {
 				success: function() {
-					new GetStartedView();
+					new SelectCourseView();
 					new LoginInfo();
+					var query = new Parse.Query(Profile);
+					query.equalTo('User', Parse.User.current().id);
+					console.log("searching for user", Parse.User.current().id)
+					query.find({
+						success:function(results){
+							results = results || [];
+							console.log("go profile results: ", results);
+							if(results.length < 1){
+								console.log("saving new profile");
+								Profile.Create().save({
+									success: function(profile){
+										Parse.User.Profile = profile;
+									}
+								});
+								
+							} else {
+								Parse.User.Profile = results[0];
+							}
+						},
+						failure: function(){
+							
+						}
+					})
 					self.undelegateEvents();
-					delete self;
 				},
 
 				error: function() {
@@ -140,10 +257,14 @@ $(function(){
 
 			Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
 				success: function() {
-					new GetStartedView();
-					new LoginInfo();
-					self.undelegateEvents();
-					delete self;
+					var profile = new Profile.create().save({
+						success: function(){
+							new SelectCourseView();
+							new LoginInfo();
+							self.undelegateEvents();		
+						}
+					});
+					
 				},
 
 				error: function(user, error) {
@@ -187,7 +308,6 @@ $(function(){
 			this.$el.html('');
 			new LogInView();
 			this.undelegateEvents();
-			delete this;
 		},
 	});
 
@@ -197,7 +317,7 @@ $(function(){
 	if(!Parse.User.current()){
 		new LogInView();
 	} else {
-		new GetStartedView();
+		new SelectCourseView();
 		new LoginInfo();
 	}
 })
