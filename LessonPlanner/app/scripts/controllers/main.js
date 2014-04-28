389a80065ea4271ac13365eb4ce5e088b992f990
Eric Wooley <ericwooley@gmail.com>
2014-04-27 22:03:25 -0700

    Moved webapp to it's own git repo on bitbucket

--- LessonPlanner/app/scripts/controllers/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('lessonPlannerApp')
-  .controller('MainCtrl', function ($scope) {
-		Parse.initialize('29wOKycz8F6JaRXHNttPXiynbGOFKUqOYAZvKZVc', '4iNIqeEGthAqjMAhYRIz5JVF1hKDnsTBDCPomf5o');
-  });
